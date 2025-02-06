5eaeacf0da8989995528e7c5207d9a89c752d242
Liam Power <liamfpower@gmail.com>
2025-02-07 09:54:33 +1100

    add env script to add sh files to path

--- convert_moises_to_musdb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python scripts/moises_to_musdb.py \
-  --src_dir  /media/stepincto/moisesdb/moisesdb_v0.1\
-  --dest_dir /media/stepincto/moisesdbmusdb 
--- inference_bsroformer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python inference.py \
---model_type bs_roformer \
---config_path ./pretrained/config_bs_roformer_384_8_2_485100.yaml \
---start_check_point pretrained/model_bs_roformer_ep_17_sdr_9.6568.ckpt \
---input_folder input/ \
---store_dir separation_results/ \
---force_cpu
